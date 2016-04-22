c9a2006e45030dd86b26a2e31f1ef561fbc02984
zhuqinlu <zero_zql@163.com>
2016-04-22 10:48:43 +0800

    no message

---
+++ ShareSDKUtils/ShareSDKUtils/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShareSDKUtils
+//
+//  Created by zhuqinlu on 16/4/22.
+//  Copyright © 2016年 zero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
